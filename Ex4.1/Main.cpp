bb8c2e19a0310a9a10f6927eda71bce17e7a400c
文昊 胡 <hwh19981124@hotmail.com>
2019-04-26 09:32:04 +0800

    right

--- Ex4.1/Main.cpp
+++ Ex4.1/Main.cpp
@@ -1,65 +1,119 @@
 #include <iostream>
 #include <vector>
 #include "glut.h"
-class CoordinateXY
+static float fRotateTri = 0;
+static float fRotateCube = 0;
+class CoordinateXYZ
 {
 public:
-	CoordinateXY(int x_, int y_);
+	CoordinateXYZ(int x_, int y_, int z_);
 	int get_CoorX();
 	int get_CoorY();
+	int get_CoorZ();
 private:
 	int CoorX;
 	int CoorY;
+	int CoorZ;
 };
 
-CoordinateXY::CoordinateXY(int x_, int y_):
+CoordinateXYZ::CoordinateXYZ(int x_, int y_, int z_):
 	CoorX(x_),
-	CoorY(y_)
+	CoorY(y_),
+	CoorZ(z_)
+
 {
 }
 
-int CoordinateXY::get_CoorX()
+int CoordinateXYZ::get_CoorX()
 {
 	return CoorX;
 }
 
-int CoordinateXY::get_CoorY()
+int CoordinateXYZ::get_CoorY()
 {
 	return CoorY;
 }
 
+int CoordinateXYZ::get_CoorZ()
+{
+	return CoorZ;
+}
 
+std::vector<CoordinateXYZ> Coordinates;
 
 void displayFunc();
+void Idlefunc();
+void RenderATriangle();
+//TODO finish this
+void RenderACube();
+
 int main() {
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	Coordinates.push_back(CoordinateXYZ(-40, -40,0));
+	Coordinates.push_back(CoordinateXYZ(40, -40,0));
+	Coordinates.push_back(CoordinateXYZ(0, 40,0));
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(600, 300);
 	glutCreateWindow("Split-Screen");
-
+	glEnable(GL_DEPTH_TEST);
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(-100, 100, -100, 100);
+	//gluOrtho2D(-100, 100, -100, 100);
+	glOrtho(-100, 100, -100, 100,-100, 100);
 
 	glMatrixMode(GL_MODELVIEW);
 
 	glutDisplayFunc(displayFunc);
+	glutIdleFunc(Idlefunc);
 	glutMainLoop();
 }
 
+
 void displayFunc()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glLoadIdentity();
 	glViewport(0, 0, 600, 300);
 	glColor3f(1, 0, 0);
 	glBegin(GL_LINES);
-	glVertex2f(0, 150);
-	glVertex2f(0, -150);
+	glVertex3f(0, 150,0);
+	glVertex3f(0, -150,0);
 	glEnd();
 
+	RenderATriangle();
 
+	glutSwapBuffers();
+}
+
+void Idlefunc()
+{
+	fRotateTri += 0.05f;
+	fRotateCube += 0.05f;
+	if (fRotateTri>360)
+	{
+		fRotateTri = 0;
+	}
+	if (fRotateCube > 360)
+	{
+		fRotateCube = 0;
+	}
+	displayFunc();
+}
+
+void RenderATriangle()
+{
+	glColor3f(0.6, 0.6, 0.6);
 	glLoadIdentity();
-	glFlush();
+	glRotatef(fRotateTri, 0, 1, 0);
+	glViewport(0, 0, 300, 300);
+	glBegin(GL_TRIANGLES);
+	for (auto iter = Coordinates.begin(); iter != Coordinates.end(); iter++)
+	{
+
+		glVertex3f(iter->get_CoorX(), iter->get_CoorY(), iter->get_CoorZ());
+	}
+	glEnd();
 }
