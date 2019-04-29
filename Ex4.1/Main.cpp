c0a1cfd0fd443cc52f94592c127716a1d4dc4313
文昊 胡 <hwh19981124@hotmail.com>
2019-04-29 20:39:14 +0800

    Quad finish

--- Ex4.1/Main.cpp
+++ Ex4.1/Main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "glut.h"
 static float fRotateTri = 0;
-static float fRotateCube = 0;
+static float fRotateQuad = 0;
 class CoordinateXYZ
 {
 public:
@@ -16,7 +16,7 @@ private:
 	int CoorZ;
 };
 
-CoordinateXYZ::CoordinateXYZ(int x_, int y_, int z_):
+CoordinateXYZ::CoordinateXYZ(int x_, int y_, int z_) :
 	CoorX(x_),
 	CoorY(y_),
 	CoorZ(z_)
@@ -39,18 +39,23 @@ int CoordinateXYZ::get_CoorZ()
 	return CoorZ;
 }
 
-std::vector<CoordinateXYZ> Coordinates;
-
+std::vector<CoordinateXYZ> Triangle_Coordinates;
+std::vector<CoordinateXYZ> Quad_Coordinates;
 void displayFunc();
 void Idlefunc();
 void RenderATriangle();
 //TODO finish this
-void RenderACube();
+void RenderAQuad();
 
 int main() {
-	Coordinates.push_back(CoordinateXYZ(-40, -40,0));
-	Coordinates.push_back(CoordinateXYZ(40, -40,0));
-	Coordinates.push_back(CoordinateXYZ(0, 40,0));
+	Triangle_Coordinates.push_back(CoordinateXYZ(-40, -40, 0));
+	Triangle_Coordinates.push_back(CoordinateXYZ(40, -40, 0));
+	Triangle_Coordinates.push_back(CoordinateXYZ(0, 40, 0));
+
+	Quad_Coordinates.push_back(CoordinateXYZ(-20, -20, 0));
+	Quad_Coordinates.push_back(CoordinateXYZ(-20, 20, 0));
+	Quad_Coordinates.push_back(CoordinateXYZ(20, 20, 0));
+	Quad_Coordinates.push_back(CoordinateXYZ(20, -20, 0));
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(100, 100);
@@ -61,7 +66,7 @@ int main() {
 
 	glMatrixMode(GL_PROJECTION);
 	//gluOrtho2D(-100, 100, -100, 100);
-	glOrtho(-100, 100, -100, 100,-100, 100);
+	glOrtho(-100, 100, -100, 100, -100, 100);
 
 	glMatrixMode(GL_MODELVIEW);
 
@@ -79,26 +84,26 @@ void displayFunc()
 	glViewport(0, 0, 600, 300);
 	glColor3f(1, 0, 0);
 	glBegin(GL_LINES);
-	glVertex3f(0, 150,0);
-	glVertex3f(0, -150,0);
+	glVertex3f(0, 150, 0);
+	glVertex3f(0, -150, 0);
 	glEnd();
 
 	RenderATriangle();
-
+	RenderAQuad();
 	glutSwapBuffers();
 }
 
 void Idlefunc()
 {
 	fRotateTri += 0.05f;
-	fRotateCube += 0.05f;
-	if (fRotateTri>360)
+	fRotateQuad += 0.05f;
+	if (fRotateTri > 360)
 	{
 		fRotateTri = 0;
 	}
-	if (fRotateCube > 360)
+	if (fRotateQuad > 360)
 	{
-		fRotateCube = 0;
+		fRotateQuad = 0;
 	}
 	displayFunc();
 }
@@ -110,9 +115,23 @@ void RenderATriangle()
 	glRotatef(fRotateTri, 0, 1, 0);
 	glViewport(0, 0, 300, 300);
 	glBegin(GL_TRIANGLES);
-	for (auto iter = Coordinates.begin(); iter != Coordinates.end(); iter++)
+	for (auto iter = Triangle_Coordinates.begin(); iter != Triangle_Coordinates.end(); iter++)
 	{
 
+		glVertex3f(iter->get_CoorX(), iter->get_CoorY(), iter->get_CoorZ());
+	}
+	glEnd();
+}
+
+void RenderAQuad()
+{
+	glColor3f(0.4, 0.5, 0.6);
+	glLoadIdentity();
+	glRotatef(fRotateQuad, 1, 0, 0);
+	glViewport(300, 0, 300, 300);
+	glBegin(GL_QUADS);
+	for (auto iter = Quad_Coordinates.begin(); iter != Quad_Coordinates.end(); iter++)
+	{
 		glVertex3f(iter->get_CoorX(), iter->get_CoorY(), iter->get_CoorZ());
 	}
 	glEnd();
